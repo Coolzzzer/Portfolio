d478d2fde28628e4e5bd0d8600f616dd12685b8c
Coolzzer <andrei.zhuk936@gmail.com>
2024-03-24 23:37:58 +0300

    v.2.0

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//скролл по кнопке из heder
+
 const hederItemOff = document.querySelector("#hederItemOff");
 const hederItemOn = document.querySelector("#hederItemOn");
 const linkX = document.querySelector("#linkX");
@@ -28,3 +30,74 @@ document.querySelectorAll('a[href^="#"').forEach(linkk => {
 document.querySelector('#linkName').addEventListener('click', function() {
 	location.reload();
 });
+
+//прокрутка навыков свайпом 
+const sliderStacks = document.querySelector('.sliderStacks');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+sliderStacks.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - sliderStacks.offsetLeft;
+  scrollLeft = sliderStacks.scrollLeft;
+});
+
+sliderStacks.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+sliderStacks.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+sliderStacks.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - sliderStacks.offsetLeft;
+  const walk = (x - startX) * 2;
+  sliderStacks.scrollLeft = scrollLeft - walk;
+});
+
+sliderStacks.addEventListener('touchstart', (e) => {
+  isDown = true;
+  startX = e.touches[0].clientX - sliderStacks.offsetLeft;
+  scrollLeft = sliderStacks.scrollLeft;
+});
+
+sliderStacks.addEventListener('touchend', () => {
+  isDown = false;
+});
+
+sliderStacks.addEventListener('touchmove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.touches[0].clientX - sliderStacks.offsetLeft;
+  const walk = (x - startX) * 2;
+  sliderStacks.scrollLeft = scrollLeft - walk;
+});
+
+
+//прокрутка навыков колесиком мыши
+let scrollPos = 0;
+
+sliderStacks.addEventListener('wheel', (e) => {
+  e.preventDefault();
+  
+  scrollPos += e.deltaY;
+  
+  sliderStacks.scrollLeft = scrollPos;
+	
+});
+
+
+
+
+
+let currentImage = 1;
+
+    function changeImage() {
+			document.getElementById(`image${currentImage}`).style.transform = "translateX(-100%)";
+        currentImage = currentImage % 7 + 1;
+        document.getElementById(`image${currentImage}`).style.transform = "translateX(0)";
+    }
